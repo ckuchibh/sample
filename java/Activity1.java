82516fd809d27e40dd21ee1591bca429611ebc9d
RamyaChilukuri <75238388+RamyaChilukuri@users.noreply.github.com>
2020-12-01 09:48:04 +0530

    Add files via upload

---
+++ java/Activity1.java
@@ -0,0 +1,18 @@
+package JavaActivity1;
+
+public class Activity1 {
+
+	public static void main(String[] args) {
+		Car honda = new Car();
+		honda.make=2014;
+		honda.color="Black";
+		honda.transmission="Manual";
+		
+		honda.displayCharacteristics();
+		honda.accelarate();
+		honda.brake();
+		
+
+	}
+
+}
